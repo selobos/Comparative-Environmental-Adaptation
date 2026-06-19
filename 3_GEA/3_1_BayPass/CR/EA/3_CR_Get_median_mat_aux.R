f58c3bc4b00e6d954298d87cf0d3b92bfcaccdf6
Simon Lobos <lobossimon@gmail.com>
2026-06-19 19:36:02 +1000

    Script

--- 3_GEA/3_1_BayPass/CR/EA/3_CR_Get_median_mat_aux.R
+++ 3_GEA/3_1_BayPass/CR/EA/3_CR_Get_median_mat_aux.R
@@ -1,17 +1,17 @@
 #Set directory
-setwd("~Desktop/Comparative-Landscape-Genomics/3_GEA/3_1_BayPass/BM/EA")
+setwd("~Desktop/Comparative-Landscape-Genomics/3_GEA/3_1_BayPass/CR/EA")
 
 ###Calculate means for aux runs and output a csv file with only those above BF20
 myList<- vector(mode = "list", length = 5)
 
 for (i in 1:5) {
-  myList[[i]]<-as.matrix(read.table(paste("bm_aux.",i,"_summary_betai.out",sep=""), header=T))
+  myList[[i]]<-as.matrix(read.table(paste("cr_aux.",i,"_summary_betai.out",sep=""), header=T))
 }
 
 bfmedian <- sapply(1:ncol(myList[[1]]), function(j) {apply(do.call(cbind,lapply(myList,`[`,,j)), 1, median)})
 colnames(bfmedian) <-colnames(myList[[1]])
 
 bfmedian<-as.data.frame(bfmedian)
-write.csv(bfmedian, file=paste0("bm_aux_median_summary_betai.out.csv"))
+write.csv(bfmedian, file=paste0("cr_aux_median_summary_betai.out.csv"))
 
 uniq_snpss=bfmedian[,"MRK"][which(bfmedian[,"BF.dB."]>20)] 
