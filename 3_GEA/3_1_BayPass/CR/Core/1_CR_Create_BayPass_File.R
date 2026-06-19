11a749562135779de24cb373a0996bbc98226d16
Simon Lobos <lobossimon@gmail.com>
2026-06-19 18:58:42 +1000

    Script

---
+++ 3_GEA/3_1_BayPass/CR/Core/1_CR_Create_BayPass_File.R
@@ -0,0 +1,63 @@
+## Convert genlight to BayPass file ##
+#Code adapted from Ahrens et al. (2021).https://doi.org/10.1111/1755-0998.13351
+
+#load packages
+library(gdata)     
+
+setwd("~/Desktop/Comparative-Landscape-Genomics/3_GEA/3_1_BayPass/CR/Core")
+
+#Load genlight object
+load("~/Desktop/Comparative-Landscape-Genomics/1_SNP_quality_control_filtering/CR/CR_gle.rdata")
+
+#Convert genlight object to data frame 
+CR_df <- as.data.frame(CR_gle)
+write.table(CR_df, "CR_012.txt", quote = FALSE)
+
+#Import LONGLAT file, this should have all individuals in the same order as INPUT file
+ind <- gle_order@ind.names
+write.csv(ind, file = "CR_ind.csv")
+
+pop <- gle_order@pop
+write.csv(pop, file = "CR_pop.csv")
+
+loc.names <- gle_order@loc.names
+write.csv(loc.names, file = "CR_loc.names.csv")
+
+longlat <- read.csv("CR_lonlat.csv", header = TRUE)
+
+#Create function for converting to BayPass file 
+baypass_format <- function(INPUTFILE,LONGLAT,BAYPASSFILE){
+  gen<-read.table(INPUTFILE, header = T, row.names=1)
+  lonlat = LONGLAT
+  allele1 = apply(gen, 2, function(snp) {
+    split(snp, lonlat$Population) %>%
+      sapply(function(genos) {
+        n = sum(!is.na(genos)) * 2
+        s = sum(genos, na.rm=T)
+        return (s)
+      })
+  })
+  allelecount = apply(gen, 2, function(snp) {
+    split(snp, lonlat$Population) %>%
+      sapply(function(genos) {
+        n = sum(!is.na(genos)) * 2
+        s = sum(genos, na.rm=T)
+        return (n)
+      })
+  })
+  allelefreq = apply(gen, 2, function(snp) {
+    split(snp, lonlat$Population) %>%
+      sapply(function(genos) {
+        n = sum(!is.na(genos)) * 2
+        s = sum(genos, na.rm=T)
+        return (s / n)
+      })
+  })
+  allele2<-allelecount-allele1
+  whole<-interleave(allele1,allele2)
+  baypass<-t(whole)
+  write.table(baypass,BAYPASSFILE, col.names = F, row.names = F, sep="\t")
+}
+
+# Run baypass conversion function
+baypass_format(INPUTFILE = "CR_012.txt", LONGLAT = longlat, BAYPASSFILE = "CR_Baypass")
